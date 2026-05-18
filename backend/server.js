35f2ca374208b02b5aa577b3410ee2fab7656f02
rohansutar01rs <rohansutar01rs@gmail.com>
2026-05-18 10:23:20 +0530

    Fixed backend data API

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,19 @@
-require("dotenv").config();
-
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
-
-const Data = require("./models/Data");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+const data = require("./jsondata.json");
 
-app.get("/", (req, res) => {
-  res.send("API Running");
-});
-
-app.get("/api/data", async (req, res) => {
-  try {
-    const data = await Data.find();
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
+app.get("/api/data", (req, res) => {
+  res.json(data);
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server Started on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
