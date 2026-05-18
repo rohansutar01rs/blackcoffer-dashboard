abd5979cddab1632c74b87eaf15c7473f6b56d7d
rohansutar01rs <rohansutar01rs@gmail.com>
2026-05-18 09:52:40 +0530

    fixed frontend api endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -60,7 +60,7 @@ function App() {
     useState("");
 
   useEffect(() => {
-   axios.get("https://blackcoffer-dashboard-lwpw.onrender.com/data")
+   axios.get("https://blackcoffer-dashboard-lwpw.onrender.com/api/data")
       .then((res) => {
         setData(res.data);
       })
